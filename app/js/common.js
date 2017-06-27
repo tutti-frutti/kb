0e083579b9d3c754b9e39431daab346ceb065fde
artem <zracing@ukr.net>
2017-06-27 17:55:37 +0300

    data

--- app/js/common.js
+++ app/js/common.js
@@ -15,7 +15,6 @@ function setMinAndMaxRooms(roomsElement, guestsNumber) {
 guests.value = 2;
 setMinAndMaxRooms(rooms, guests.value);
 rooms.value = rooms.min;
-rooms.value = 5;
 
 guests.onchange = function() {
     setMinAndMaxRooms(rooms, guests.value);
@@ -26,4 +25,35 @@ formElement.onsubmit = function(evt) {
     
     document.cookie = 'guests=' + guests.value;
     document.cookie = 'rooms=' + rooms.value;
+}
+
+var arr = [1,2,3];
+
+arr.forEach(function(item, index, array){
+    console.log(item, index, array);
+});
+
+var container = document.querySelector('.hotels-list');
+
+// 1. перебрать все элементы в структуре данных.
+// перебрать данные нужно для того, чтобы создать для каждого элемента DOM-элемент на основе шаблона. 
+// для этого запускаем цикл по js-массиву.
+// внутри перебора массива запускаем Ф. getElementFromTemplate
+// на вход Ф. принимает объект (data), который представляет собой объект описывающий данные для отеля
+// данные находятся в hotel.js
+// была ошибка в том, что переменная hotels была не определена. дело в том, что hotels.js находился ниже, чем файл, который его вызывал.
+hotels.forEach(function(hotel){
+    var element = getElementFromTemplate(hotel);
+    console.log(element);
+    console.log(hotel);
+    container.appendChild(element);
+});
+
+// 2. для каждого элемента создать DOM-элемент на основе шаблона
+/**
+*@param {Object} data
+*@return {Element} data
+*/
+function getElementFromTemplate(data) {
+    return document.createElement('article');
 }
---
+++ app/js/hotels.js
@@ -0,0 +1,46 @@
+var hotels = [
+    {
+        'amenities': ['breakfast'],
+        'distance': 3.8,
+        'location': {
+            'lat': 35.5971291676,
+            'lng': 139.7117990255
+        },
+        'name': 'Tokyo Inn',
+        'pictures': [],
+        'price': 3200,
+        'rating': 7.4,
+        'stars': 3,
+        'preview': 'aaa'
+    },
+    
+    {
+        'amenities': ['breakfast'],
+        'distance': 4.8,
+        'location': {
+            'lat': 35.5971291676,
+            'lng': 139.7117990255
+        },
+        'name': 'First Inn',
+        'pictures': [],
+        'price': 3200,
+        'rating': 8.2,
+        'stars': 4,
+        'preview': 'www'
+    },
+    
+    {
+        'amenities': ['breakfest'],
+        'distance': 3.8,
+        'location': {
+            'lat': 35.5971291676,
+            'lng': 139.7117990255
+        },
+        'name': 'Japan',
+        'pictures': [],
+        'price': 5700,
+        'rating': 8.9,
+        'stars': 5,
+        'preview': 'aaa'
+    },
+]
