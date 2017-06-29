7097acc0cff4023febcead47d7085534c31d955b
tutti-frutti <buzzloud.origin@gmail.com>
2017-06-29 23:00:06 +0300

    template

--- app/js/common.js
+++ app/js/common.js
@@ -44,9 +44,6 @@ var container = document.querySelector('.hotels-list');
 // была ошибка в том, что переменная hotels была не определена. дело в том, что hotels.js находился ниже, чем файл, который его вызывал.
 hotels.forEach(function(hotel){
     var element = getElementFromTemplate(hotel);
-//    console.log(getElementFromTemplate);
-//    console.log(element);
-//    console.log(hotel);
     container.appendChild(element);
 });
 
@@ -61,5 +58,33 @@ hotels.forEach(function(hotel){
 // как параметр data видит объекты в hotels.js?
 // почему в .forEach объект называется hotel - это произвольное название? значения data подставляются автоматически?
 function getElementFromTemplate(data) {
-    return document.createElement('article');
+    var template = document.querySelector('#hotel-template');
+//    var element = template.content.children[0].cloneNode(true);
+    if ('content' in template) {
+        var element = template.content.children[0].cloneNode(true);
+    } else {
+        var element = template.children[0].cloneNode(true);
+    }
+    
+    element.querySelector('.hotel-name').textContent = data.name;
+    element.querySelector('.hotel-rating').textContent = data.rating;
+    element.querySelector('.hotel-price-value').textContent = data.price;
+    
+    return element;
 }
+
+// 'content' in template вернет true если template является объектом DocumentFragment, в противном случае - нет и мы будем знать что это IE.
+// таким образом это будет обычным тегом. в этом случае мы обращаемся к var element = template.children[0].cloneNode(true); "напрямую" и сам template придется задать display: none;
+
+// скопируем шаблон
+// заполним данными
+// вставим
+
+// var template = document.querySelector('#hotel-template'); --- получаем шаблон - содержимое тега template. затем нужно положить его содержимое во внутрь созд.элем.
+// var element = template.content.children[0].cloneNode(true);
+// template.content --- template представляет собой элемент, все, что в нём записано хранится в свойстве .content - это особенность браузеров, которые поддеживают тег template. контент этого тега - это DocumentFragment
+// .children[0] --- это первый потомок (тег article) DocumentFragment т.е. тега template
+// .cloneNode(true) --- клонируем со всем содержимым (за это отвечает свойство true).
+
+//element.querySelector('.hotel-name').textContent = data.name;
+// .textContent --- возвращает только текст. т.е. мы можем читать и записывать этот текст. а далее записываем в него то, что записанно в данных под ключем name.
